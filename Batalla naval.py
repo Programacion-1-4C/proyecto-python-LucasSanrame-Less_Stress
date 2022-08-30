46497925d9e1b2a57dbeacc6771ee9b1b39f23c2
LucasSanrame <110994205+LucasSanrame@users.noreply.github.com>
2022-08-31 00:47:33 +0200

    Update Batalla naval.py

--- Batalla naval.py
+++ Batalla naval.py
@@ -1,10 +1,14 @@
 from random import sample
 universal = ()
+def no_paso():
+  print("*nada paso*")
+def hundido():
+  print("*EXPLOCION*")
 print("Hola, bienvenido a Batalla naval")
 print()
 #respuesta_1 = input("¿Quieres empezar un juego nuevo?: -" )
 respuesta_1 = input("¿Quieres jugar batalla naval?: -")
-if respuesta_1 == "si":
+if respuesta_1 == "si" or universal == "si":
     while universal == "si" or respuesta_1 == "si":
      dificultad = print("Como dificultad tienes: Facil, Medio, Dificil y Locura")
      seleccion_de_dificultad = input("¿Cual quieres elegir? -")
@@ -19,14 +23,14 @@ if respuesta_1 == "si":
         while barcos_restantes != 0:
          seleccion = input("Seleccione un cuadrante entre el a1 y el j10: -")
          if seleccion in d:
-           print("*EXPLOSION*")
+           hundido()
            print(posiciones[d[0]])
            barcos_restantes -= 1
            if barcos_restantes == 0:
                print("Gracias por jugar")
                universal = input("¿Quieres jugar otra vez? -")
          else:
-           print("*nada paso*")
+           no_paso()
      if seleccion_de_dificultad == "medio":
         posiciones = {}
         barcos_restantes = 20       
@@ -38,14 +42,14 @@ if respuesta_1 == "si":
         while barcos_restantes != 0:
            seleccion = input("Seleccione un cuadrante entre el a1 y el j10: -")
            if seleccion in d:
-             print("*EXPLOSION*")
+             hundido()
              print(posiciones[d[0]])
              barcos_restantes -= 1
              if barcos_restantes == 0:
                  print("Gracias por jugar")
                  universal = input("¿Quieres jugar otra vez? -")
            else:
-             print("*nada paso*")
+             no_paso()
      if seleccion_de_dificultad == "dificil":
         posiciones = {}
         barcos_restantes = 10
@@ -57,14 +61,14 @@ if respuesta_1 == "si":
         while barcos_restantes != 0:
            seleccion = input("Seleccione un cuadrante entre el a1 y el j10: -")
            if seleccion in d:
-             print("*EXPLOSION*")
+             hundido()
              print(posiciones[d[0]])
              barcos_restantes -= 1
              if barcos_restantes == 0:
                  print("Gracias por jugar")
                  universal = input("¿Quieres jugar otra vez? -")
            else:
-             print("*nada paso*")
+              no_paso()
      if seleccion_de_dificultad == "locura":
         posiciones = {}
         barcos_restantes = 5
@@ -76,18 +80,13 @@ if respuesta_1 == "si":
         while barcos_restantes != 0:
            seleccion = input("Seleccione un cuadrante entre el a1 y el j10: -")
            if seleccion in d:
-             print("*EXPLOSION*")
+             hundido()
              print(posiciones[d[0]])
              barcos_restantes -= 1
              if barcos_restantes == 0:
                  print("Gracias por jugar")
                  universal = input("¿Quieres jugar otra vez? -")
-                 if universal == "no":
-                     seguro = input("¿Estas seguro? -")
-                     if seguro == "si":
-                         break
-                         print("Gracias por jugar")
            else:
-             print("*nada paso*")
+              no_paso()
 else:
     print("Que tenga un buen dia")
